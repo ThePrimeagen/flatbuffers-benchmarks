05279172f71dbbe9b27e61be9bf65bdaea908439
Michael Paulson <michael.b.paulson@gmail.com>
2016-08-25 19:36:48 -0700

    fix(console.log): more perf :)

--- src/node/perf/tcp/FramingStream.js
+++ src/node/perf/tcp/FramingStream.js
@@ -31,31 +31,24 @@ FramingStream.prototype._transform = function _transform(chunk, enc, cb) {
     // auto assumes that the first 32 bits is an unsigned int.
     let frameMark = 0;
     const chunkLength = chunk.length;
-
-    console.log('chunk');
     
     // Why a doWhile?  Because they are awesome.
     do {
-        console.log('chunk#start', frameMark, chunk.length);
         
         // Buf is empty, therefore there is no previous results.
         // Therefore, we must initialize our aggregator.
         if (this._buf === null) {
             frameMark = this._initializeAggregator(chunk, frameMark);
             if (frameMark === null) {
-                console.log('case frameMark#NULL');
                 break;
             }
-            console.log('frameMark', frameMark);
         }
         
 
         const remainingLength = chunkLength - frameMark;
-        console.log('chunk#start-remainingLength', remainingLength, this._len, this._totalLength);
 
         // We must frame this stream with the next incoming data.
         if (this._totalLength > this._len + remainingLength && remainingLength) {
-            console.log('case1', chunk.slice(frameMark).toString());
             
             this._bufs.push(chunk);
             this._bufs.push(frameMark);
@@ -66,7 +59,6 @@ FramingStream.prototype._transform = function _transform(chunk, enc, cb) {
 
         // What remains in this chunk is the data we expect.
         else if (this._totalLength === this._len + remainingLength) {
-            console.log('case2');
             
             // Pass the remaining data to the next item.
             const data = this._aggregate(chunk, frameMark);
@@ -79,7 +71,6 @@ FramingStream.prototype._transform = function _transform(chunk, enc, cb) {
 
         // There is more than one message in this chunk.
         else if (remainingLength) {
-            console.log('case3');
             const endIndex = frameMark + (this._totalLength - this._len);
             const aggregatedData = this._aggregate(chunk, frameMark, endIndex);
             
@@ -89,7 +80,6 @@ FramingStream.prototype._transform = function _transform(chunk, enc, cb) {
             frameMark = endIndex;
         }
 
-        console.log('chunk#end', frameMark, chunk.length);
     } while (frameMark < chunk.length);
     
     cb();
@@ -131,7 +121,6 @@ FramingStream.prototype._aggregate = function _aggregate(chunk, startIndex, endI
         buf = Buffer.allocUnsafe(this._totalLength);
     }
 
-    console.log('Bufs', bufs);
     // Base case, the chunk is the message.
     if (bufs.length === 0) {
         if (startIndex === 0 && endIndex === chunk.length) {
@@ -142,21 +131,15 @@ FramingStream.prototype._aggregate = function _aggregate(chunk, startIndex, endI
 
     // Add all previous bufs to the beginning.
     let idx = 0;
-    console.log('bufs.length', bufs.length);
     for (let i = 0; i < bufs.length; i += 2) {
         const b = bufs[i];
         const bStartIndex = bufs[i + 1];
 
-        console.log('copy(', b, ', ', idx, ', ', bStartIndex, ')');
-        console.log('contents', b.slice(startIndex).toString());
         b.copy(buf, idx, bStartIndex);
 
         idx += b.length - bStartIndex;
     }
 
-    console.log('buf.before', buf.toString(), idx, startIndex, endIndex);
-    console.log('chunk', chunk.slice(startIndex, endIndex).toString());
-    console.log('bytes copied', chunk.copy(buf, idx, startIndex, endIndex));
-    console.log('buf.after', buf.toString());
+    chunk.copy(buf, idx, startIndex, endIndex);
     return buf;
 };
