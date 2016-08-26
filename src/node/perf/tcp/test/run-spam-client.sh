86e4860198e4867ad96d41995118a455a878d836
Michael Paulson <michael.b.paulson@gmail.com>
2016-08-26 09:28:08 -0700

    spam clients.

--- src/node/perf/tcp/test/run-spam-client.sh
+++ src/node/perf/tcp/test/run-spam-client.sh
@@ -5,20 +5,31 @@ export COLUMNS=$2
 
 CLIENT_ID=$3
 IS_JSON=$4
-COUNT=$5
 
 COUNTER=0
 ID=0
 
-while [ $COUNTER -lt "$COUNT" ]; do
+while [ $COUNTER -lt 2 ]; do
     ID=$(($COUNTER + $CLIENT_ID))
-    echo "sh spam-client.sh 5000 $ID $IS_JSON &"
-    sh spam-client.sh 5000 $ID $IS_JSON &
+    echo "sh spam-client.sh $5 $ID $IS_JSON &"
+    sh spam-client.sh $5 $ID $IS_JSON 33333 &
 
     COUNTER=$(($COUNTER + 1))
 done
 
-# Blocks on this one.
+while [ $COUNTER -lt 4 ]; do
+    ID=$(($COUNTER + $CLIENT_ID))
+    echo "sh spam-client.sh $5 $ID $IS_JSON &"
+    sh spam-client.sh $5 $ID $IS_JSON 33334 &
+
+    COUNTER=$(($COUNTER + 1))
+done
+
 ID=$(($COUNTER + $CLIENT_ID))
-sh spam-client.sh 5000 $ID $IS_JSON
+ID_2=$(($COUNTER + $CLIENT_ID + 1))
+echo "sh spam-client.sh $5 $ID $IS_JSON &"
+sh spam-client.sh $5 $ID $IS_JSON 33335 &
+
+echo "sh spam-client.sh $5 $ID_2 $IS_JSON "
+sh spam-client.sh $5 $ID_2 $IS_JSON 33335
 
--- src/node/perf/tcp/test/spam-client.sh
+++ src/node/perf/tcp/test/spam-client.sh
@@ -4,6 +4,6 @@ len=$1
 counter=0
 
 while [ $counter -lt $len ]; do
-    curl -s http://$SERVER_ADDR:33333/lolomo/$2/$ROWS/$COLUMNS/0/false/$3 > /dev/null
+    curl -s http://$SERVER_ADDR:$4/lolomo/$2/$ROWS/$COLUMNS/0/false/$3 > /dev/null
     counter=$(($counter+1))
 done
