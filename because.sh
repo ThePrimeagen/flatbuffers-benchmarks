9fbf187559ef9679135645d01e3c88e54f75d6ab
Michael Paulson <michael.b.paulson@gmail.com>
2016-08-26 09:08:58 -0700

    updated runscript.

--- because.sh
+++ because.sh
@@ -2,9 +2,9 @@
 cd src/node/perf/tcp/test
 
 if [ "$1" == "lolomo" ]; then
-    node run-lolomo-as-a-service.js --port=33334
+    node run-lolomo-as-a-service.js --port=$2
 elif [ "$1" == "ratings" ]; then
-    node run-ratings-as-a-service.js --port=33335
+    node run-ratings-as-a-service.js --port=$2
 elif [ "$1" == "agg" ]; then
-    node run-lolomo-ratings-aggregation-as-a-service.js --lolomoHost=$2 --ratingsHost=$3 --host=0.0.0.0
+    node run-lolomo-ratings-aggregation-as-a-service.js --port=$2 --ratingsPort=$2 --lolomoPort=$2 --lolomoHost=$3 --ratingsHost=$4 --host=0.0.0.0
 fi
